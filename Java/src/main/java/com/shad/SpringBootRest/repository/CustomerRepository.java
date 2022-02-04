a96b9c97526d117dda91f68ab5623df56b056479
ahuja-sagar <sagarahuja101@gmail.com>
2022-02-04 15:35:18 +0530

    Final Project

---
+++ Java/src/main/java/com/shad/SpringBootRest/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.shad.SpringBootRest.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shad.SpringBootRest.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+	
+	Optional<Customer> findByEmailAndPassword(String email, String password);
+
+}
